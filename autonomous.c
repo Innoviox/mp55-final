07dffa61ca03ea22c45202951c8fa59e9b182dbc
Simon Chervenak <sarras305@gmail.com>
2018-06-10 19:08:56 -0400

    Update autonomous.c

--- autonomous.c
+++ autonomous.c
@@ -1,16 +1,12 @@
 void autonomous() {
-	pointTurnLeft(calc_time(45, 50), 50);
-	backwards(2000, 50);
-	pointTurnRight(calc_time(180, 50), 50);
-	backwards(1000, 50);
-	moveFlag();
-	moveBack();
-	takeTemperature();
-	pointTurnRight(calc_time(90, 50), 50);
-	forwards(500, 50);
-	moveFlag();
-	takeTemperature();
-	takeSalinity();
-	moveBack();
-	pointTurnRight(calc_time(135, 50), 50);
+	pointTurnLeft(calc_time(90, 50), 50);
+    forwards(2000, 50);
+    takeTemperature();
+    moveFlag();
+    takeTemperature();
+    takeSalinity();
+    moveBack();
+    pointTurnRight(calc_time(180, 50), 50);
+    forwards(2000, 50);
+    pointTurnLeft(calc_time(90, 50), 50);
 }
