73418ae57e12b304b952d94ec3d219a4d9bf7645
Simon Chervenak <sarras305@gmail.com>
2018-06-09 20:05:35 -0400

    Fixed the thing

--- master.c
+++ master.c
@@ -30,21 +30,21 @@ task main()
     int count = 0;
     while (1==1) {
         bool stay = true;
-        
-        if (vexRT[Btn8L] == 1) {
-            findSun(sunValues);
-            isData = false;
-        } else if (vexRT[Btn8U] == 1) {
-            takeWind();
-            isData = false;
-        } else if (vexRT[Btn8R] == 1) {
-            takeSalinity();
-            isData = false;
-        } else if (vexRT[Btn8D] == 1) {
-            takeTemperature();
-            isData = false;
-        }
-        if (isDrive) {
+        if (isData) {
+            if (vexRT[Btn8L] == 1) {
+                findSun(sunValues);
+                isData = false;
+            } else if (vexRT[Btn8U] == 1) {
+                takeWind();
+                isData = false;
+            } else if (vexRT[Btn8R] == 1) {
+                takeSalinity();
+                isData = false;
+            } else if (vexRT[Btn8D] == 1) {
+                takeTemperature();
+                isData = false;
+            }
+        } else if (isDrive) {
             turn(vexRT[Ch1] / -2);
             forwards(vexRT[Ch3] / -2);
             if (vexRT[Btn7U] == 1) {
@@ -96,6 +96,10 @@ task main()
         
         if (vexRT[Btn7L] == 1) {
             isDrive = !isDrive;
+        }
+        
+        if (vexRT[Btn8U] == 1) {
+            isData = true;
         }
     }
 }
