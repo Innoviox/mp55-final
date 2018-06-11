e14b8938f3aa0ca361a89776d714f92d9777f94c
Matthew Shu <32606558+Nonameentered@users.noreply.github.com>
2018-06-11 11:12:19 -0400

    Update autonomous.c

--- autonomous.c
+++ autonomous.c
@@ -1,9 +1,11 @@
 void autonomous() {
-    Sun sunValue;
-    findSun(sunValue);
     takeWind();
     pointTurnLeft(calc_time(45, 50), 50);
     backwards(2000, 50);
     pointTurnRight(calc_time(180, 50), 50);
     backwards(1000, 50);
+    wait1MSecs(1000);
+    forwards(1000, 50);
+    Sun sunValue;
+    findSun(sunValue);
 }
